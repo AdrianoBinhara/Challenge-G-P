504e0107e433b2f787ef438a77bd46951c8ee8ac
Adriano D'lucca Binhara <62717202+AdrianoBinhara@users.noreply.github.com>
2022-01-15 23:38:04 -0300

    corrigindo heroku

--- GPInventory/GP_API/Startup.cs
+++ GPInventory/GP_API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace GP_API
 {
@@ -21,14 +22,22 @@ namespace GP_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });     
             services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase(databaseName: "database_name"));
             services.AddControllers();
             services.AddScoped<IInventoryService, InventoryService>();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
